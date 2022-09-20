d3983d5d6fbe0322549ae9ff6cded220380e4dcc
x4FF3 <like.the.23@gmail.com>
2022-09-20 12:50:11 +0000

    first part

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+ fn calculate_price_of_apples(amount: i32) -> i32 {
+    if amount <= 40{
+        return amount * 2;
+    } else {
+        return amount;
+    }
+    
+ }
 // Don't modify this function!
 #[test]
 fn verify_test() {
